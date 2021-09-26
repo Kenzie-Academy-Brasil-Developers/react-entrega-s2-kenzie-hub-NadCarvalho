ebace5e5ce33a13b9a89d23e051a6d9e8856cc27
NadCarvalho <carvalho.nadiacaroline@gmail.com>
2021-09-26 18:45:08 -0300

    feat: added session management and router to app

--- src/App.js
+++ src/App.js
@@ -1,17 +1,39 @@
-import { Switch, Router } from 'react-router-dom';
+import { Switch, Route, Redirect, useHistory } from 'react-router-dom';
 import Register from './Pages/Register';
 import Home from './Pages/Home';
 import Welcome from './Pages/Welcome';
-
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [loggedIn, setLoggedIn] = useState(null);
+  const history = useHistory();
+
+  const login = (user) => {
+    localStorage.setItem('user', JSON.stringify(user));
+    setLoggedIn(user);
+    history.push('/home');
+  };
+
+  useEffect(() => {
+    const storedUser = localStorage.getItem('user');
+    if (storedUser) {
+      login(JSON.parse(storedUser));
+    }
+  });
+
   return (
     <div className="App">
       <Switch>
-        <Router exact path="/register" component={Register} />
-        <Router exact path="/home" component={Home} />
-        <Router path="*" component={Welcome} />
+        <Route exact path="/register">
+          {loggedIn ? <Redirect to="/home" /> : <Register onRegister={login} />}
+        </Route>
+        <Route exact path="/home">
+          {!loggedIn ? <Redirect to="/" /> : <Home/>}
+        </Route>
+        <Route path="*">
+          {loggedIn ? <Redirect to="/home" /> : <Welcome onLogin={login}/>}
+        </Route>
       </Switch>
     </div>
   );
